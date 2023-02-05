c0f104d101610261cb9862ca00517316179d2f68
baghy <baghya14@gmail.com>
2023-02-05 14:51:50 +0000

    JIRA 001

---
+++ src/test/java/uk/axone/devintest/gittest/AppTest.java
@@ -0,0 +1,11 @@
+package uk.axone.devintest.gittest;
+
+import org.junit.Test;
+
+public class AppTest {
+
+        @Test
+                public void test1(){
+            System.out.println("Running test 1");
+    }
+}
